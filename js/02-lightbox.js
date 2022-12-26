9b09afb5e4f121a94050e45e0c7c419c9d90b681
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2022-12-26 19:03:55 +0200

    Update 02-lightbox.js
    Good

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,54 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryUl = document.querySelector(".gallery");
-console.log(galleryUl);
 
 const Markup = createMarkup (galleryItems);
-console.log(Markup);
 
 galleryUl.insertAdjacentHTML("beforeend", Markup);
 
-galleryUl.addEventListener(`click`, evt => {
-    evt.preventDefault();
-//     const instance = basicLightbox.create(`
-//     <img src="${evt.target.dataset.source}" width="800" height="600">
-// `)
-
-// instance.show()
-       
-    }
- )
-    
-
-function galleryDivClick(evt) {
-    if (evt.target.classList.contains("gallery")) {
-    }
-  
-}
-
 function createMarkup(galleryItems) {return galleryItems.map(
   ({ preview, original, description }) =>
-    `<div class="gallery__item">
-    <a class="gallery__link" href="${original}">
-        <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-            style.width = "100px"
-        />
-    </a>
-</div>`
-)
+`<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+   )
+
 .join("");
 }
+var lightbox = new SimpleLightbox('.gallery a', {  });
 
-console.log(Markup);
-
-
-
-
-console.log(SimpleLightbox);
-
-
-console.log(galleryItems);
